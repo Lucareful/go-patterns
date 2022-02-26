1c3974642c4d33fc756db90f1843762ed87d0a87
luenci <lucarful@gmail.com>
2022-02-26 19:37:17 +0800

    feat: 新增 Uber 推荐的 options 模式

--- options模式/main.go
+++ options模式/main.go
@@ -28,10 +28,47 @@ func NewPerson(name string, age int, options ...Options) *Person {
 	return p
 }
 
+// uber 推荐的模式暴露接口
+
+type Person2 struct {
+	Name  string
+	Age   int
+	Class string
+}
+
+type Options2 interface {
+	apply(*Person2)
+}
+
+type Class string
+
+func (c Class) apply(p *Person2) {
+	p.Class = string(c)
+}
+
+func WithClass2(class string) Options2 {
+	return Class(class)
+}
+
+func NewPerson2(name string, age int, options ...Options2) *Person2 {
+	p := &Person2{
+		Name: name,
+		Age:  age,
+	}
+	for _, option := range options {
+		option.apply(p)
+	}
+	return p
+}
+
 func main() {
 	p1 := NewPerson("张三", 18)
 	p2 := NewPerson("李四", 18, WithClass("三班"))
+	p3 := NewPerson2("张三", 18)
+	p4 := NewPerson2("李四", 18, WithClass2("三班"))
 
 	fmt.Printf("%v\n", p1)
 	fmt.Printf("%v\n", p2)
+	fmt.Printf("%v\n", p3)
+	fmt.Printf("%v\n", p4)
 }
